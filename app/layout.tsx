bc412cf2213bb0af6ca1149640bd39f5dacb64f1
MehriddinBarnoyev <149578450+MehriddinBarnoyev@users.noreply.github.com>
2024-11-29 23:22:13 +0500

    1-day error is fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
-          <MainNavbar />
+          <MainNavbar/>
           {children}
         </ThemeProvider>
       </body>
